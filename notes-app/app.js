eb6c43a12eef06d1112dcff6459aa0620ef24602
Mjnari <mjnarir@gmail.com>
2019-03-31 22:29:58 -0500

    finished lesson 22

--- notes-app/app.js
+++ notes-app/app.js
@@ -21,7 +21,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -30,7 +30,7 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function(argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -38,7 +38,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function(){
+    handler(){
         console.log('Listing out all notes')
     }
 })
@@ -46,7 +46,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function(){
+    handler(){
         console.log('Reading a note')
     }
 })
